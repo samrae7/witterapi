b1496ad4a017e7beb0b169d92072e77a6ee4aeee
Sam Rae <samuelrae@gmail.com>
2019-06-02 22:45:26 +0100

    add filmController and routes

---
+++ src/routes/film-routes.ts
@@ -0,0 +1,25 @@
+import {FilmController} from "../controller/FilmController";
+
+const filmRoutes = [{
+  method: "get",
+  route: "/films",
+  controller: FilmController,
+  action: "all"
+}, {
+  method: "get",
+  route: "/films/:id",
+  controller: FilmController,
+  action: "one"
+}, {
+  method: "post",
+  route: "/films",
+  controller: FilmController,
+  action: "save"
+}, {
+  method: "delete",
+  route: "/films/:id",
+  controller: FilmController,
+  action: "remove"
+}];
+
+export default filmRoutes;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,4 @@
 import userRoutes from "./user-routes";
-export const Routes = [...userRoutes];
+import filmRoutes from "./film-routes";
+
+export const Routes = [...userRoutes, ...filmRoutes];
